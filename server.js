4c607762a5b080aabd38f1af61883729ff656dee
Antonio Campos <antj.campos@gmail.com>
2015-12-16 20:11:06 +0000

    Show timestamp on UI

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var PORT = process.env.PORT || 3000;
+var moment = require('moment');
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
@@ -6,20 +7,22 @@ var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function (socket) {
-	console.log('User connected via socket.io!');
+io.on('connection', function(socket) {
+    console.log('User connected via socket.io!');
 
-	socket.on('message', function (message) {
-		console.log('message received: ' + message.text);
+    socket.on('message', function(message) {
+        console.log('message received: ' + message.text);
 
-		io.emit('message', message);
-	});
+        message.timestamp = moment().valueOf();
+        io.emit('message', message);
+    });
 
-	socket.emit('message', {
-		text: 'Welcome to the chat application!'
-	});
+    socket.emit('message', {
+        text: 'Welcome to the chat application!',
+        timestamp: moment().valueOf()
+    });
 });
 
-http.listen(PORT, function () {
-	console.log('Server started!');
+http.listen(PORT, function() {
+    console.log('Server started!');
 });
